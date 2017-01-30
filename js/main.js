1216deaf106846771fb76b1ef5ae22b500d43f8d
Supreet Chahal <supreet_chahal@hotmail.com>
2017-01-30 14:20:18 -0800

    Javascript updated

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
-$.ajax({
+$(document).ready(function() {
 
-  method: 'GET',
-  url: 'https://api.nytimes.com/svc/topstories/v2/' + select + '.json?api-key=e6dd6cfbfd7c4e748aa37e3491eb34d8'
-}).done(function(data) {
+    $('.selection').change('.selection option:selected', function(event) {
+      event.preventDefault();
 
-    if (data.results.length === 0) {
-      $('.story-grid').append("<p>There are no stories in this section.</p>");
-    }
-    else {
-      let news = data.results;
-      news = news.filter(function(item) {
-        return item.multimedia.length;
-      }).splice(0, 12);
+        var categorySelected = $('.selection option:selected').text().toLowerCase();
+
+
+        var url = 'https://api.nytimes.com/svc/topstories/v2/' + categorySelected + '.json';
+        url += '?' + $.param({
+            'api-key': 'e6dd6cfbfd7c4e748aa37e3491eb34d8'
+        });
+
+        $.ajax({
+                method: 'GET',
+                url: url
+            })
+
+            .done(function(data) {
+                var listItem = '';
+                var news = data.results;
+                var filteredNews = news.filter(function(item) {
+                    return item.multimedia.length > 0;
+                }).slice(0, 12);
+
+                $.each(filteredNews, function(item, index) {
+                    var img = index.multimedia[4].url;
+                    listItem += '<li class="itemImage" style="background-image:url(' + img + ')"><p>' + index.abstract + '</p></li>';
+                    listItem += '<a href="' + index.url + '">';
+                    listItem += '</a></li>';
+                })
+
+                $('.stories').append(listItem);
+
+            });
+    });
+});
